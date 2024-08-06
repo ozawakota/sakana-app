92e3afc88bfa42914acf40429d9db438c440d195
k_ozawa <kouta.ozawa@nadia.bz>
2024-08-06 16:29:48 +0900

    transitionアニメーション

---
+++ sakana/src/app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Header from "./header";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Page Transition",
+  description: "Using framer-motion to add trasition between pages",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Header />
+        <main>{children}</main>
+      </body>
+    </html>
+  );
+}
