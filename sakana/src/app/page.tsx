db4c262119b3a4953d0c7c05a0a5cb38e228f45f
k_ozawa <kouta.ozawa@nadia.bz>
2024-08-23 18:11:49 +0900

    alertボタン

--- sakana/src/app/page.tsx
+++ sakana/src/app/page.tsx
@@ -1,14 +1,16 @@
+"use client"
+
+import { Button } from "@/components/ui/button"
+
 export default function Home() {
   return (
     <section className="p-4">
       <div className="inner">
         <div className="flex justify-center md:w-[900px] mx-auto gap-[50px]">
-          <div>
-            <p className="text-white md:text-2xl">通貨</p>
-          </div>
-          <div>
-            <p className="text-white md:text-2xl">通貨</p>
-          </div>
+          <Button>ボタン</Button>
+          <Button size="lg" variant="secondary" onClick={() => { alert('aleart')}}>Alert!</Button>
+          <Button >Save</Button>
+          <Button >ボタン</Button>
         </div>
       </div>
     </section>
