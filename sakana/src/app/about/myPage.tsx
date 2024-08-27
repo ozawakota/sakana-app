72a0cb00a9d8bfd0db12e885cedb54ef608ebe26
k_ozawa <kouta.ozawa@nadia.bz>
2024-08-27 19:19:22 +0900

    toast削除

--- sakana/src/app/about/myPage.tsx
+++ sakana/src/app/about/myPage.tsx
@@ -1,11 +1,6 @@
 "use client"
 
-import { Button } from "@/components/ui/button"
-import { ToastAction } from "@/components/ui/toast"
-import { useToast } from "@/components/ui/use-toast"
-
 export default function About() {
-  const { toast } = useToast()
 
   return (
     <section className="p-4">
@@ -26,20 +21,6 @@ export default function About() {
             </dd>
           </dl>
         </div>
-        <Button
-          variant="outline"
-          onClick={() => {
-            toast({
-              title: "Scheduled: Catch up ",
-              description: "Friday, February 10, 2023 at 5:57 PM",
-              action: (
-                <ToastAction altText="Goto schedule to undo">Undo</ToastAction>
-              ),
-            })
-          }}
-        >
-          Add to calendar
-        </Button>
       </div>
     </section>
   );
